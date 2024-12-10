68e6448215805fb1cb51d8ffadc4dd66744554bf
Nitai Charan <nitaicharan@gmail.com>
2024-12-10 20:02:01 +0000

    feat: add `--gpu` temperature option on `statusnotification` script

--- scripts/statusnotification.sh
+++ scripts/statusnotification.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Initialize variables to store which data will be shown
-show_workspace=false
 show_date_time=false
 show_volume=false
 show_ram_usage=false
 show_disk_usage=false
 show_cpu_usage=false
 show_cpu_temperature=false
+show_gpu_temperature=false
 
 # Check if a title was provided
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 \"title\" [--workspace] [--date] [--cpu] [--ram] [--disk] [--volume] [--all]"
+  echo "Usage: $0 \"title\" [--date] [--cpu] [--ram] [--disk] [--volume] [--gpu] [--all]"
   exit 1
 fi
 
@@ -21,12 +21,12 @@ shift # Shift the arguments so the remaining ones are flags
 
 # Use getopt to handle long options including --all
 OPTIONS=$(getopt -o "" \
-    --long workspace,date,cpu,ram,disk,volume,all \
+    --long date,cpu,ram,disk,volume,gpu,all \
     -- "$@")
 
 # If getopt fails (invalid options), exit with an error
 if [ $? -ne 0 ]; then
-  echo "Usage: $0 \"title\" [--workspace] [--date] [--cpu] [--ram] [--disk] [--volume] [--all]"
+  echo "Usage: $0 \"title\" [--date] [--cpu] [--ram] [--disk] [--volume] [--gpu] [--all]"
   exit 1
 fi
 
@@ -36,10 +36,6 @@ eval set -- "$OPTIONS"
 # Parse the options
 while true; do
   case "$1" in
-    --workspace)
-      show_workspace=true
-      shift
-      ;;
     --date)
       show_date_time=true
       shift
@@ -60,14 +56,18 @@ while true; do
       show_volume=true
       shift
       ;;
+    --gpu)
+      show_gpu_temperature=true
+      shift
+      ;;
     --all)
-      show_workspace=true
       show_date_time=true
       show_volume=true
       show_ram_usage=true
       show_disk_usage=true
       show_cpu_usage=true
       show_cpu_temperature=true
+      show_gpu_temperature=true
       shift
       ;;
     --)
@@ -75,30 +75,26 @@ while true; do
       break
       ;;
     *)
-      echo "Usage: $0 \"title\" [--workspace] [--date] [--cpu] [--ram] [--disk] [--volume] [--all]"
+      echo "Usage: $0 \"title\" [--date] [--cpu] [--ram] [--disk] [--volume] [--gpu] [--all]"
       exit 1
       ;;
   esac
 done
 
 # Fetch system data
-workspace=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true) | .name')
 date_time=$(date +"%d (%A) %B %H:%M")
 volume=$(amixer get Master | grep -oP '[0-9]+(?=%)' | head -n 1)
 ram_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
 disk_usage=$(df / | tail -1 | awk '{print $5}')
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-cpu_temperature=$(sensors | grep 'Package id' | awk '{print $4}')
+cpu_temperature=$(sensors | grep -E 'Tctl|Composite|temp1' | awk '{print $2}' | sed 's/+//g' | head -n 1)
+gpu_temperature=$(sensors | grep -E 'edge|temp1' | awk '{print $2}' | sed 's/+//g' | head -n 1)
 
 # Construct the message based on options
 message=""
 
 if [ "$show_date_time" = true ]; then
   message+="$date_time\n"
-fi
-
-if [ "$show_workspace" = true ]; then
-  message+="Workspace: $workspace\n"
 fi
 
 if [ "$show_volume" = true ]; then
@@ -110,7 +106,11 @@ if [ "$show_cpu_usage" = true ]; then
 fi
 
 if [ "$show_cpu_temperature" = true ]; then
-  message+="CPU temperature: ${cpu_temperature}%\n"
+  message+="CPU temperature: ${cpu_temperature}°C\n"
+fi
+
+if [ "$show_gpu_temperature" = true ]; then
+  message+="GPU temperature: ${gpu_temperature}°C\n"
 fi
 
 if [ "$show_ram_usage" = true ]; then
@@ -128,3 +128,4 @@ else
   echo "No information to display. Use options to select what to show."
   exit 1
 fi
+
